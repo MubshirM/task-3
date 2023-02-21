5fe91604fe00d66eeca721cf264c1436db3a44f0
MubshirM <mubshirmmm@gmail.com>
2023-02-21 08:43:50 -0800

    first commit

---
+++ task 3.py
@@ -0,0 +1,13 @@
+def add_too(num1,num2):
+    return num1+num2
+first=int(input("enter first number:"))
+last=int(input("enter last number:"))
+total=first+last
+
+
+print(total)
+def age_in_days(age):
+    days_in_year = 365
+    return age * days_in_year
+print(age_in_days(25))
+
